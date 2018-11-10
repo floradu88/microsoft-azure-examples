7efbd6dd2605130379309e6d1144dc844a199404
Radu Florescu <radu.florescu@hotmail.com>
2018-11-10 16:25:09 +0200

    Added initial structure.

---
+++ src/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureExample.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
